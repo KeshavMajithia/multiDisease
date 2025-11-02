a84a9ddd725f7a214d6f0c8b69ded367be7039b2
keshav <keshav.22mic7006@vitapstudent.ac.in>
2025-11-02 16:58:59 +0530

    edits

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,8 @@ set -e
 # Install system dependencies
 apt-get update && apt-get install -y python3-dev gcc
 
+# Upgrade pip and install build tools
+pip install --upgrade pip setuptools wheel
+
 # Install Python packages
-pip install --upgrade pip
 pip install -r requirements.txt
