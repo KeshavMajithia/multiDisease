de6fbe04844bb33bfe5c257081aa1daf1b8024a4
keshav <keshav.22mic7006@vitapstudent.ac.in>
2025-11-02 17:08:23 +0530

    Simplify Vercel deployment configuration

--- backend/build.sh
+++ backend/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install system dependencies
-apt-get update && apt-get install -y python3.9 python3.9-dev python3.9-venv gcc
-
-# Set Python 3.9 as default
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
-
-# Install Python packages
-pip3 install --upgrade pip setuptools wheel
-pip3 install -r requirements.txt
+# Upgrade pip and install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
